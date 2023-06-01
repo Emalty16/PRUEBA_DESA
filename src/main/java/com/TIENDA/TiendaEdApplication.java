4d6352152fabac6ea727a074346c9113f93c95fa
LABORATORIO 04 <LABORATORIO 04@DESKTOP-JO2MKAB>
2023-05-31 18:18:05 -0600

    Inicio del proyecto

---
+++ src/main/java/com/TIENDA/TiendaEdApplication.java
@@ -0,0 +1,13 @@
+package com.TIENDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEdApplication.class, args);
+	}
+
+}
